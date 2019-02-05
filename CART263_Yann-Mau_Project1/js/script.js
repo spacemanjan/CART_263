29a12bea6072dbc986fdaa9eeac0be11a1a92b90
Yann-Maurice McNiven <Yanmaumcniven@gmail.com>
2019-02-05 02:11:32 -0500

    Added Sisyphus animation+sounds+keypressing
    Added Sisyphus animation that at the moment will be triggered by key presses (which I've also added). Eventually Sisyphus will change his position depending on how long you can keep the chain of good keys going. To keep it more in line with the myth of sisyphus the essay maybe if you hit a certain number of correct keys sisyphus will be enlightened and then remain there with nothing to do...

--- CART263_Yann-Mau_Project1/js/script.js
+++ CART263_Yann-Mau_Project1/js/script.js
@@ -8,6 +8,7 @@ author, and this description to match your project!
 
 ******************/
 let r;
+let sisStage;
 
 let thoughts = [
  "Hmmmm that's a good thought",
@@ -22,6 +23,12 @@ function setup(){
 //call function every second to generate a random number
 setInterval(random,1000);
 $trigger = $('#triggerZone');
+$sisy = $('#sisy');
+
+setInterval(keyPress,1000);
+//ISSUE WITH PLAYING MUSIC uncaught DOMException
+$('audio#track')[0].play();
+
 //====SUGGESTION====//
 //make 4 triggerzones for each corresponding Arrow
 //make triggerzone droppable so it can catch draggable objects
@@ -30,19 +37,32 @@ $trigger.droppable({
 	tolerance: "touch",
 	// when a draggable is OVER the triggerZone activate key press function
 	over: function(event,ui){
-		keyPress();
+
 	}
 });
 }
 
 //key press will check if the corresponding key is down and if so...
+//TO DO: add music play everytime you hit the key correctly
 function keyPress(){
-	if ($trigger.keydown(38)){
+	$(window).keydown(function(evt){
+		if (evt.which === 38){
+			setInterval( sisyphus, 400);
+			sisStage = 1;
+		console.log("UP GOT");
+	} else if (evt.which === 40){
+		setInterval( sisyphus, 400);
+		sisStage = 2;
+		console.log("DOWN GOT");
+	} else if (evt.which === 37){
+		console.log("LEFT GOT");
+	} else if (evt.which === 39){
+		console.log("RIGHT GOT");
+	}
 		//make the arrow dissapear
 		//make sysphus think
-	}
+	})
 }
-
 function random(){
 //generate random number to determine which arrow is sent out
  r = Math.floor((Math.random()*4)+1);
@@ -54,13 +74,14 @@ addArrow();
 function addArrow(){
 	//create new arrow div
 	let $arrow = $('<img></img>');
+	$arrow.addClass
 	//if r = 1 create UP arrow
 	//give it the ID of ONE & a corresponding image
 	// make it draggable so that the droppable will notice it
 	if (r === 1){
 		$arrow.attr('id','one');
 		$arrow.attr('src','assets/images/Arrowup.png');
-		$arrow.animate({ bottom: "+=1100"}, 5000);
+		$arrow.animate({ top: "-=1100"},  5000);
 		$arrow.draggable();
 		//===ERROR===//
 		//my div keeps moving over to the right
@@ -87,3 +108,49 @@ function addArrow(){
 // spawn arrows in smartzone
 	$arrow.appendTo("#smartZone");
 }
+
+function sisyphus(){
+if (sisStage === 1){
+	if ($sisy.attr('src') === "assets/images/Sysphis1.png") {
+		$sisy.attr('src', 'assets/images/Sysphis1breath.png');
+	}
+	else {
+		$sisy.attr('src', 'assets/images/Sysphis1.png');
+	}
+}
+if (sisStage === 2){
+	if ($sisy.attr('src') === "assets/images/Sysphis1.png") {
+		$sisy.attr('src', 'assets/images/Sysphis2.png').delay(200);
+	}
+	else if ($sisy.attr('src') === "assets/images/Sysphis2.png"){
+		$sisy.attr('src', 'assets/images/Sysphis3.png').delay(200);
+	}
+	else if ($sisy.attr('src') === "assets/images/Sysphis3.png"){
+		$sisy.attr('src', 'assets/images/Sysphis4.png').delay(200);
+	}
+	else if ($sisy.attr('src') === "assets/images/Sysphis4.png"){
+		$sisy.attr('src', 'assets/images/Sysphis5.png').delay(200);
+	}
+	if ($sisy.attr('src') === "assets/images/Sysphis5.png") {
+		console.log("im in stage 3")
+		sisStage = 3;
+	}
+}
+if (sisStage === 3){
+	if ($sisy.attr('src') === "assets/images/Sysphis5.png") {
+		$sisy.attr('src', 'assets/images/Sysphis4.png').delay(200);
+	}
+	else if ($sisy.attr('src') === "assets/images/Sysphis4.png"){
+		$sisy.attr('src', 'assets/images/Sysphis3.png').delay(200);
+	}
+	else if ($sisy.attr('src') === "assets/images/Sysphis3.png"){
+		$sisy.attr('src', 'assets/images/Sysphis2.png').delay(200);
+	}
+	else if ($sisy.attr('src') === "assets/images/Sysphis2.png"){
+		$sisy.attr('src', 'assets/images/Sysphis1.png').delay(200);
+	}
+	if ($sisy.attr('src') === "assets/images/Sysphis1.png") {
+		console.log("im in stage 1")
+		sisStage = 2;
+	}
+}}
