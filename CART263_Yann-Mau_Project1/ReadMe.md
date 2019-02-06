5ea2f4cb92d0c2546d540d1793d5913095ab8596
Yann-Maurice McNiven <Yanmaumcniven@gmail.com>
2019-02-06 00:29:06 -0500

    Project 1 - Finished

---
+++ CART263_Yann-Mau_Project1/ReadMe.md
@@ -0,0 +1,30 @@
+Think Think Revolution by Yann-Maurice McNiven
+
+In Think Think Revolution you work to help Sisyphus discover the meaning of life, an ultimately endless effort.
+Much like in his original task Sisyphus is doomed to ponder on the details and mysteries of life for all eternity, but
+that doesn't mean we can't have fun while we help him. The player in this case is the force of inspiration, and by
+hitting the correct keys you inspire Sisyphus. Nowhere does it say that the game is un-winnable, it is endless, and
+so for the player the only way to escape the curse of Sisyphus is to stop playing and stop dwelling on this question.
+Just as Sisyphus is trapped by the gods to perform feats that are endless, so is the player trapped by the Creator,
+ trying to achieve the untenable task of being awakened in lucidity to the complexity of the human experience.
+Sisyphus' character has been contemplated, lionized and mythologized by thinkers such as Camus to explain our own
+existential dread, but in playing this game, one understands the futility of the endeavor.  I am interested in how our emancipation
+is in contemplating in tandem, seeing the fractals of truth in the Other, and in togetherness coming to quell our profound fears.
+
+I based the game off of the classic arcade game Dance Dance Revolution, a game which if endless would surely be
+in the pantheon of ancient greek punishments. Hitting the corresponding keys plays an approving sound, almost
+reassuring the player to keep trying their best. Ultimately there is no score and no point to playing, Sisyphus moves
+and changes position with every five points. I did my best to create the impression of a animated greek pottery scene,
+the effect relying on the classic orange and black colors often seen in that art.
+
+I had a lot of difficulty with figuring out how to locate the element on the page, this was my main obstacle in making
+the game. The challenge was my deciding on using a .draggable & .droppable effect on my Arrows and Static arrows.
+I also was having a lot of difficulty locating individual arrows, most of my time was spent on creating the fundamental
+game mechanics; then when running into a roadblock and having exhausted my conceivable google searches I put it
+aside to ask for help in person, I moved onto creating the next feature and made the best use of my time that way.
+
+The game finally came together after doing some in class work with Professor Pippin Barr who reminded me of the $('this')
+selector and enlightened me as to how to properly use .offset. The final effect was to calculate position from the top of .each
+arrow in the class up/down/left/right arrow and subtracting that from the static arrow. If the arrow was within the range and
+the directional arrow was clicked then a point is scored and the arrow changes images and plays a sound. Overall I'm pleased
+with the result and this has given me a far better understanding of Jquery & Jquery UI.
--- CART263_Yann-Mau_Project1/js/script.js
+++ CART263_Yann-Mau_Project1/js/script.js
@@ -1,156 +1,240 @@
 /*****************
 
-Title of Project
-Author Name
+Think Think Revolution - Sisyphus Edition
+Yann-Maurice McNiven
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Sisyphus has put down his stone to contemplate like the philosophers of his culture on
+the meaning of life, unfortunately for sisphys like his classic challenge this will be an
+unachievable task. Hit the keys as they correspond to inspire sisyphus' thoughts and
+at the same time try and contemplate the greater mysteries yourself and live the
+sisyphus experience.
 
 ******************/
-let r;
-let sisStage;
 
+//declaring my variables
+let r;
+let sisStage = 1;
+let score = 0;
+//load my sounds
+let soundtrack = new Audio( "assets/sounds/AgeOfMythology.wav" );
+let upsound = new Audio( "assets/sounds/1stTone.wav" );
+let downsound = new Audio( "assets/sounds/2ndTone.wav" );
+let leftsound = new Audio( "assets/sounds/3rdTone.wav" );
+let rightsound = new Audio( "assets/sounds/4thTone.wav" );
+
+//create an array with thoughts to be loaded in by the life() function
 let thoughts = [
- "Hmmmm that's a good thought",
- "smart thinking",
- "Yup that makes sense to me",
- "if that is true than it must infer that..."
+	"Plato might have known",
+	"A good sandwhich",
+	"Life and death",
+	"Living every day",
+	"Letting life take the reins",
+	"Allowing oneself to trust",
+	"Being open to new experiences",
+	"Smelling the roses",
+	"Treating yourself",
+	"Enjoying every detail",
+	"pushing a large stone",
+	"suffering for others",
+	"giving up control",
+	"in being honest",
+	"playing video games",
+	"Pippin might know the answer",
+	"Worshiping zeus",
+	"maybe there is no answer"
 ];
 
-$(document).ready(setup);
-
-function setup(){
-//call function every second to generate a random number
-setInterval(random,1000);
-$trigger = $('#triggerZone');
-$sisy = $('#sisy');
-
-setInterval(keyPress,1000);
-//ISSUE WITH PLAYING MUSIC uncaught DOMException
-$('audio#track')[0].play();
-
-//====SUGGESTION====//
-//make 4 triggerzones for each corresponding Arrow
-//make triggerzone droppable so it can catch draggable objects
-$trigger.droppable({
-	//set the tolerance to touch so if the key even overlaps the zone a little it will still be caught
-	tolerance: "touch",
-	// when a draggable is OVER the triggerZone activate key press function
-	over: function(event,ui){
-
-	}
-});
+//preload function
+$( document ).ready( preload );
+//load game on click
+function preload(){
+	$('HTML').click(setup);
 }
 
-//key press will check if the corresponding key is down and if so...
-//TO DO: add music play everytime you hit the key correctly
-function keyPress(){
-	$(window).keydown(function(evt){
-		if (evt.which === 38){
-			setInterval( sisyphus, 400);
-			sisStage = 1;
-		console.log("UP GOT");
-	} else if (evt.which === 40){
-		setInterval( sisyphus, 400);
-		sisStage = 2;
-		console.log("DOWN GOT");
-	} else if (evt.which === 37){
-		console.log("LEFT GOT");
-	} else if (evt.which === 39){
-		console.log("RIGHT GOT");
-	}
-		//make the arrow dissapear
-		//make sysphus think
-	})
-}
-function random(){
-//generate random number to determine which arrow is sent out
- r = Math.floor((Math.random()*4)+1);
- console.log(r);
-//send out arrow
-addArrow();
+function setup() {
+	$('HTML').off();
+	//hide instructions by adding class gone which has visibility: hidden in css
+	$('.Instructions').addClass('gone');
+	//call function every second to generate a random number
+	setInterval( random, 1000 );
+	//shorthand for the sisyphus animation
+	$sisy = $( '#sisy' );
+	//setup keySetup so it's listening for our key presses
+	keySetup();
+
+	//start the soundtrack
+	//declare loop as true so it repeats constantly
+	soundtrack.loop = true;
+	soundtrack.currentTime = 0;
+	soundtrack.play();
+
+	//setInterval for the animation of sisyphus
+	setInterval( sisyphus, 400 );
 }
 
-function addArrow(){
+//key Setup will check if the corresponding key is down and check where it is in
+//relation to the static key if its within the range it will change the arrow to ressemble the
+//static arrow and make a corresponding sound
+function keySetup() {
+	$( window ).keydown( function( evt ) {
+		if ( evt.which === 38 ) {
+			$( '.uparrow' ).each( function() {
+				//if the arrow is within 60px difference up and down from the center of the static arrows then
+				//play sound, change img src & add a point to the score
+				if ( ( $( this ).offset().top ) - ( $( '#uphold' ).offset().top ) <= 30 && ( $( this ).offset().top ) - ( $( '#uphold' ).offset().top ) >= -30 ) {
+					upsound.currentTime = 0;
+					upsound.play();
+					$( this ).attr( 'src', 'assets/images/uphold.png' );
+					score += 1;
+					console.log( score );
+				}
+			} )
+		} else if ( evt.which === 40 ) {
+			$( '.downarrow' ).each( function() {
+				if ( ( $( this ).offset().top ) - ( $( '#downhold' ).offset().top ) <= 30 && ( $( this ).offset().top ) - ( $( '#downhold' ).offset().top ) >= -30 ) {
+					downsound.currentTime = 0;
+					downsound.play();
+					$( this ).attr( 'src', 'assets/images/downhold.png' );
+					score += 1;
+				}
+			} )
+		} else if ( evt.which === 37 ) {
+			$( '.leftarrow' ).each( function() {
+				if ( ( $( this ).offset().top ) - ( $( '#lefthold' ).offset().top ) <= 30 && ( $( this ).offset().top ) - ( $( '#lefthold' ).offset().top ) >= -30 ) {
+					leftsound.currentTime = 0;
+					leftsound.play();
+					$( this ).attr( 'src', 'assets/images/lefthold.png' );
+					score += 1;
+				}
+			} )
+		} else if ( evt.which === 39 ) {
+			$( '.rightarrow' ).each( function() {
+				if ( ( $( this ).offset().top ) - ( $( '#righthold' ).offset().top ) <= 30 && ( $( this ).offset().top ) - ( $( '#righthold' ).offset().top ) >= -30 ) {
+					rightsound.currentTime = 0;
+					rightsound.play();
+					$( this ).attr( 'src', 'assets/images/righthold.png' );
+					score += 1;
+				}
+			} )
+		}
+	} )
+}
+
+function random() {
+	//generate random number to determine which arrow is sent out
+	r = Math.floor( ( Math.random() * 4 ) + 1 );
+	//send out arrow
+	addArrow();
+}
+
+//generate arrows
+function addArrow() {
 	//create new arrow div
-	let $arrow = $('<img></img>');
+	let $arrow = $( '<img></img>' );
 	$arrow.addClass
 	//if r = 1 create UP arrow
-	//give it the ID of ONE & a corresponding image
-	// make it draggable so that the droppable will notice it
-	if (r === 1){
-		$arrow.attr('id','one');
-		$arrow.attr('src','assets/images/Arrowup.png');
-		$arrow.animate({ top: "-=1100"},  5000);
-		$arrow.draggable();
-		//===ERROR===//
-		//my div keeps moving over to the right
+	//give it the class of up/down/left/right arrow & a corresponding image
+	//when the arrow completes it's animation remove it
+	if ( r === 1 ) {
+		$arrow.attr( 'class', 'uparrow' );
+		$arrow.attr( 'src', 'assets/images/Arrowup.png' );
+		$arrow.animate( {
+			top: "-=1100"
+		}, 5000, function() {
+			$( this ).remove();
+		} );
 	}
 	// repeat same process with other arrows
-	if(r === 2){
-		$arrow.attr('id','two');
-		$arrow.attr('src','assets/images/Arrowdown.png');
-		$arrow.animate({ bottom: "+=1100"}, 5000);
-		// $arrow.draggable();
+	if ( r === 2 ) {
+		$arrow.attr( 'class', 'downarrow' );
+		$arrow.attr( 'src', 'assets/images/Arrowdown.png' );
+		$arrow.animate( {
+			bottom: "+=1100"
+		}, 5000, function() {
+			$( this ).remove();
+		} );
 	}
-	if(r === 3){
-		$arrow.attr('id','three');
-		$arrow.attr('src','assets/images/Arrowleft.png');
-		$arrow.animate({ bottom: "+=1100"}, 5000);
-		// $arrow.draggable();
+	if ( r === 3 ) {
+		$arrow.attr( 'class', 'leftarrow' );
+		$arrow.attr( 'src', 'assets/images/Arrowleft.png' );
+		$arrow.animate( {
+			bottom: "+=1100"
+		}, 5000, function() {
+			$( this ).remove();
+		} );
 	}
-	if(r === 4){
-		$arrow.attr('id','four');
-		$arrow.attr('src','assets/images/Arrowright.png');
-		$arrow.animate({ bottom: "+=1100"}, 5000);
-		// $arrow.draggable();
+	if ( r === 4 ) {
+		$arrow.attr( 'class', 'rightarrow' );
+		$arrow.attr( 'src', 'assets/images/Arrowright.png' );
+		$arrow.animate( {
+			bottom: "+=1100"
+		}, 5000, function() {
+			$( this ).remove();
+		} );
 	}
-// spawn arrows in smartzone
-	$arrow.appendTo("#smartZone");
+	// spawn arrows in smartzone
+	$arrow.appendTo( "#smartZone" );
 }
 
-function sisyphus(){
-if (sisStage === 1){
-	if ($sisy.attr('src') === "assets/images/Sysphis1.png") {
-		$sisy.attr('src', 'assets/images/Sysphis1breath.png');
+//Sisyphus animation function
+//Sisyphus has three stages a resting stage (stage 1), a rising stage (stage 2), and a return
+//to resting stage (stage 3), this function controls the animation frames, and depending
+//on the stage sisyphus is at what animation he should do next.
+//His animations depend on the score every 5 points he will change positions
+function sisyphus() {
+	//resting stage
+	if ( sisStage === 1 ) {
+		if ( $sisy.attr( 'src' ) === "assets/images/Sysphis1.png" ) {
+			$sisy.attr( 'src', 'assets/images/Sysphis1breath.png' );
+		} else {
+			$sisy.attr( 'src', 'assets/images/Sysphis1.png' );
+		}
 	}
-	else {
-		$sisy.attr('src', 'assets/images/Sysphis1.png');
+	//score/stage handler also triggers the life() function
+	if ( score >= 5 ) {
+		if ( sisStage === 1 ) {
+			sisStage = 2;
+		} else if ( sisStage === 2 ) {
+			sisStage = 3;
+		}
+		//
+		life();
+		console.log("LIFE");
+		score = 0;
+	}
+	if ( sisStage === 2 ) {
+		if ( ( $sisy.attr( 'src' ) === "assets/images/Sysphis1.png" ) || ( $sisy.attr( 'src' ) === "assets/images/Sysphis1breath.png" ) ) {
+			$sisy.attr( 'src', 'assets/images/Sysphis2.png' ).delay( 200 );
+		} else if ( $sisy.attr( 'src' ) === "assets/images/Sysphis2.png" ) {
+			$sisy.attr( 'src', 'assets/images/Sysphis3.png' ).delay( 200 );
+		} else if ( $sisy.attr( 'src' ) === "assets/images/Sysphis3.png" ) {
+			$sisy.attr( 'src', 'assets/images/Sysphis4.png' ).delay( 200 );
+		} else if ( $sisy.attr( 'src' ) === "assets/images/Sysphis4.png" ) {
+			$sisy.attr( 'src', 'assets/images/Sysphis5.png' ).delay( 200 );
+		}
+	}
+	if ( sisStage === 3 ) {
+		if ( $sisy.attr( 'src' ) === "assets/images/Sysphis5.png" ) {
+			$sisy.attr( 'src', 'assets/images/Sysphis4.png' ).delay( 200 );
+		} else if ( $sisy.attr( 'src' ) === "assets/images/Sysphis4.png" ) {
+			$sisy.attr( 'src', 'assets/images/Sysphis3.png' ).delay( 200 );
+		} else if ( $sisy.attr( 'src' ) === "assets/images/Sysphis3.png" ) {
+			$sisy.attr( 'src', 'assets/images/Sysphis2.png' ).delay( 200 );
+		} else if ( $sisy.attr( 'src' ) === "assets/images/Sysphis2.png" ) {
+			$sisy.attr( 'src', 'assets/images/Sysphis1.png' ).delay( 200 );
+		}
+		if ( $sisy.attr( 'src' ) === "assets/images/Sysphis1.png" ) {
+			sisStage = 1;
+		}
 	}
 }
-if (sisStage === 2){
-	if ($sisy.attr('src') === "assets/images/Sysphis1.png") {
-		$sisy.attr('src', 'assets/images/Sysphis2.png').delay(200);
-	}
-	else if ($sisy.attr('src') === "assets/images/Sysphis2.png"){
-		$sisy.attr('src', 'assets/images/Sysphis3.png').delay(200);
-	}
-	else if ($sisy.attr('src') === "assets/images/Sysphis3.png"){
-		$sisy.attr('src', 'assets/images/Sysphis4.png').delay(200);
-	}
-	else if ($sisy.attr('src') === "assets/images/Sysphis4.png"){
-		$sisy.attr('src', 'assets/images/Sysphis5.png').delay(200);
-	}
-	if ($sisy.attr('src') === "assets/images/Sysphis5.png") {
-		console.log("im in stage 3")
-		sisStage = 3;
-	}
+//Life() pulls from the array at the top and generates a new thought
+//its called every 5 points
+function life() {
+	let $think = $( '#thinking' );
+	let thought = thoughts[ Math.floor( Math.random() * thoughts.length ) ];
+	// Add a p tag to the dialog div that contains the question text
+	$think.html('The Meaning of life is...');
+	$think.append( "<p>" + thought + "</p>" );
+
 }
-if (sisStage === 3){
-	if ($sisy.attr('src') === "assets/images/Sysphis5.png") {
-		$sisy.attr('src', 'assets/images/Sysphis4.png').delay(200);
-	}
-	else if ($sisy.attr('src') === "assets/images/Sysphis4.png"){
-		$sisy.attr('src', 'assets/images/Sysphis3.png').delay(200);
-	}
-	else if ($sisy.attr('src') === "assets/images/Sysphis3.png"){
-		$sisy.attr('src', 'assets/images/Sysphis2.png').delay(200);
-	}
-	else if ($sisy.attr('src') === "assets/images/Sysphis2.png"){
-		$sisy.attr('src', 'assets/images/Sysphis1.png').delay(200);
-	}
-	if ($sisy.attr('src') === "assets/images/Sysphis1.png") {
-		console.log("im in stage 1")
-		sisStage = 2;
-	}
-}}
