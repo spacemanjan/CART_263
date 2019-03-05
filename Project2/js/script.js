835aab0f8a060f1165ed81f75f3118b3d4c42fe0
Yann-Maurice McNiven <Yanmaumcniven@gmail.com>
2019-03-05 13:36:07 -0500

    Added Project 2 folder (begining project)
    Humble beginings: here we mark the start of our journey back into Jquery and that wonderful world of programming. With a week to the deadline will our young protagonist complete on time, or will he be stopped by the dasterdly distractions that lie all around him. Stay tuned to find out.

---
+++ Project2/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*****************
+
+Children's Avatar Maker 2019 Edition
+Yann-Maurice McNiven
+
+In Children's Avatar Maker 2019 Edition, have fun exploring the possibilites of making a fun
+character of your own, combine diffrent heads to bodys, change up the legs and the arms.
+Discover over 50 diffrent combinations that can be made with this fun project.
+
+******************/
+
+let head;
