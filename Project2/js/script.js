8368d01de54779f4e16f814d069053b03a77f84b
Yann-Maurice McNiven <Yanmaumcniven@gmail.com>
2019-03-05 17:19:20 -0500

    Project 2: Update 2
    Episode 2: Revenge of the Coder.
    In this episode our protagonist has solved the issue of the stacking animations the bug has been destroyed but how? The solution was adding stopPropogation to the end of the Toggle function however although it solves the issue it mysteriously is registering in the console as a reference error.
    The images now change off screen, and are pulled randomly from an array.
    And now the third objective is being accomplished adding images to the array depending on the amount of clicks.

--- Project2/js/script.js
+++ Project2/js/script.js
@@ -12,18 +12,37 @@ Discover over 50 diffrent combinations that can be made with this fun project.
 ******************/
 
 //--------------=VARIABLES=--------------//
-//declare faces variable
-//faces is going to be used constantly
-let faces;
+//declare faces, bodies, hands, & feets arrays
+	//these arrays will hold the corresponding body part's library of images to chose from
+let faces = [
+	'assets/images/head/goomba.png',
+	'assets/images/head/mario.png'
+];
+let bodies = [
+
+];
+let hands = [
+
+];
+let feets = [
+
+];
+
+//declare head, torso, arms, & legs variables
 let $head;
 let $torso;
+let $arms;
+let $legs;
+
+//variable numClick will track how many times a body part has been toggled
+let numClick = 0;
 
 //--------------=START_PROGRAM=--------------//
 $( document ).ready( preload );
 
 //--------------=PRELOAD=--------------//
 //preload is called at the begining to make sure things are called not on load
-//but only when the user is ready to play & clicks on the screen
+	//but only when the user is ready to play & clicks on the screen
 function preload() {
 	//"on click launch setup"
 	$( 'HTML' ).click( setup );
@@ -32,39 +51,53 @@ function preload() {
 //--------------=SETUP=--------------//
 function setup() {
 	$head = $( '#head' );
-	$head.on('click', toggle);
+	$head.on( 'click', toggle );
 
 	$torso = $( '#torso' );
-	$torso.on('click', toggle);
+	$torso.on( 'click', toggle );
 }
 
 //--------------=TOGGLE=--------------//
 //toggle is the main function of Children's Avatar Maker 2019 Edition
-//$(this) is the clicked id (#head, #torso, #arms, #legs)
-//$(this) will be animated to go right off screen
+	//$(this) is the clicked id (#head, #torso, #arms, #legs)
+		//$(this) will be animated to go right off screen.
 //At the completion of the first animation the changeImages function is called
-//the css of $(this) is changed to -250 and then animated
-//back to it's original position
+	//the css of $(this) is changed to -250 and then animated
+		//back to it's original position
 function toggle() {
-	let position = $(this).position();
-	//first animation
-	$(this).animate({
+	//add 1 to the numClick variable
+	numClick += 1;
+	//first animation stage
+	$( this ).animate( {
 		left: "2000"
-	}, 500, changeImages);
-	//second animation
-	$(this).animate({
-			left:"37.5%"
-		}, 1000);
+		//changeImages is called
+	}, 500, changeImages );
+	//second animation stage
+	$( this ).animate( {
+		left: "37.5%"
+	}, 1000 );
+	console.log(numClick);
+	stopPropogation();
 }
 
 //--------------=CHANGE_IMAGES=--------------//
 //change images is the second main function which works in conjuction with TOGGLE
-//this function sets the css to the far left
+	//this function sets the css to the far left
 //then the function checks what $(this) is either (#head, #torso, #arms, #legs)
-//depending on which is selected, change the image to a new image
+	//depending on which is selected, change the image to a new image
 function changeImages() {
-	$(this).css('left', -250);
-	if( this.id === "head"){
-		$head.attr('src','assets/images/head/goomba.png');
+	let face = faces[ Math.floor( Math.random() * faces.length ) ];
+	$( this ).css( 'left', -250 );
+	if ( this.id === "head" ) {
+		$head.attr( 'src', face );
+	}
+	//call addImages function at the end of the last click
+	addImages();
+}
+
+//--------------=ADD_IMAGES=--------------//
+function addImages(){
+	if (numClick === 2){
+		console.log("muthafucka");
 	}
 }
